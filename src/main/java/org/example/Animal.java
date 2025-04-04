0a625d883685849b1913a4844374a83c16a72a64
Eric Schmidt <eric0802@web.de>
2025-04-04 14:25:32 +0200

    add record Species

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -2,8 +2,8 @@ package org.example;
 
 import java.util.UUID;
 
-public record Animal(UUID ID, String name, String species, int age, Owner owner) {
-    public Animal(String name, String species, int age, Owner owner) {
+public record Animal(UUID ID, String name, Species species, int age, Owner owner) {
+    public Animal(String name, Species species, int age, Owner owner) {
         this(UUID.randomUUID(), name, species, age, owner);
     }
 }
