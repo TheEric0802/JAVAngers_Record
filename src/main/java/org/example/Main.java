138cfc7aba628f838dc77669dcfb8e3446788766
Eric Schmidt <eric0802@web.de>
2025-04-04 14:19:20 +0200

    add record Owner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,14 @@ import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
-        Animal dog = new Animal("Bonny", "Dog", 14);
-        Animal cat = new Animal("Garfield", "Cat", 8);
-        Animal bird1 = new Animal(UUID.fromString("9ee8be82-d85f-4c6b-a3eb-f3e0e7f3ac63"),"Birdie", "Bird", 2);
-        Animal bird2 = new Animal(UUID.fromString("9ee8be82-d85f-4c6b-a3eb-f3e0e7f3ac63"),"Birdie", "Bird", 2);
+
+        Owner eric = new Owner("Eric", 25, "Mainz");
+        Owner max = new Owner("Max", 30, "München");
+
+        Animal dog = new Animal("Bonny", "Dog", 14, eric);
+        Animal cat = new Animal("Garfield", "Cat", 8, eric);
+        Animal bird1 = new Animal(UUID.fromString("9ee8be82-d85f-4c6b-a3eb-f3e0e7f3ac63"),"Birdie", "Bird", 2, max);
+        Animal bird2 = new Animal(UUID.fromString("9ee8be82-d85f-4c6b-a3eb-f3e0e7f3ac63"),"Birdie", "Bird", 2, max);
 
         System.out.println(dog);
 
